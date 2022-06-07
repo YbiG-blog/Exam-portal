35ccdc980e2feb2fe843c5a2cfc51855c2282ee0
Parkhi <parkhigarg277@gmail.com>
2022-06-07 23:44:01 +0530

    id

--- routers/addquestion.js
+++ routers/addquestion.js
@@ -14,27 +14,28 @@ const correctans = 'B';
 // post request
 router.post("/addquestion", async (req, res) => {
   try {
-    const { question, category} = await req.body;
+    const { question, category } = await req.body;
     const question_create = new Question({
       question,
       category,
-    //   options:options,
-      correctAnswer:correctans
+      //   options:options,
+      correctAnswer: correctans
     });
 
     for (let i = 0; i < options.length; i++) {
-        const element = options[i];
-        if(element == correctans){
-            question_create.iscorrect = true;
-            break;
-        }
+      const element = options[i];
+      if (element == correctans) {
+        question_create.iscorrect = true;
+        break;
+      }
         
     }
 
     const savequestion = await question_create.save();
     res.status(201).send(savequestion);
   } catch (err) {
-      console.log(err);
+    console.log(err);
+  }
 
 module.exports = router;
 
