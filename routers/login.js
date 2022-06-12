6871e6b64c586b492dcec8022b20a91f41a9d531
Parkhi <parkhigarg277@gmail.com>
2022-06-12 15:37:04 +0530

    resolving token

--- routers/login.js
+++ routers/login.js
@@ -8,40 +8,47 @@ router.get("/admin", async (req, res) => {
 });
 router.post("/login", async (req, res) => {
   try {
-  const password = req.body.password;
-  const studentNum = req.body.studentNum;
-  const user_check = await User.findOne({ studentNum: studentNum });
-  if (user_check) {
-    const matchUser_password = await bcrypt.compare(password, user_check.password );
-    const matchAdmin_password = await bcrypt.compare(password, user_check.adminPassword );
+    const password = req.body.password;
+    const studentNum = req.body.studentNum;
+    const user_check = await User.findOne({ studentNum: studentNum });
+    if (user_check) {
+      const matchUser_password = await bcrypt.compare(
+        password,
+        user_check.password
+      );
+      const matchAdmin_password = await bcrypt.compare(
+        password,
+        user_check.adminPassword
+      );
 
-   const cookie_token = await user_check.generateAuthToken();
+      const cookie_token = await user_check.generateAuthToken();
       console.log(cookie_token);
-  
+
       //add cookie
-      res.cookie("jwt_csi", cookie_token, {secure:true,
+      res.cookie("jwt_csi", cookie_token, {
+        secure: true,
         expires: new Date(Date.now() + 864000000),
         httpOnly: true,
       });
 
-    if (matchAdmin_password) {
-      user_check.isAdmin = true
-        res.status(201).send({ isAdmin: user_check.isAdmin,"token":`${cookie_token}` });
+      if (matchAdmin_password) {
+        user_check.isAdmin = true;
+        res.status(201).send({ isAdmin: "true", token: `${cookie_token}` });
+      } else if (matchUser_password) {
+        res.status(201).send({
+          // `This is verified user and token for user is : ${cookie_token}`
+          message: "User login succesfully",
+          cookie_token: cookie_token,
+        });
+      } else {
+        res.status(400).send({ msg: "Wrong Password" });
+      }
+    } else {
+      res.status(400).send({ msg: "Invalid details" });
     }
-    else if(matchUser_password){
-      res
-        .status(201)
-        .send(`This is verified user and token for user is : ${cookie_token}`);
-    }
-    else {
-      res.status(400).send({ msg: "Wrong Password" });
-    }
-  } else {
-    res.status(400).send({ msg: "Invalid details" });
+  } catch (err) {
+    console.log(err);
   }
-} catch (err) {
-   console.log(err); 
-}
 });
 
 module.exports = router;
