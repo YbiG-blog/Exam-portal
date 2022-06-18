a96f395dc2c65335b718d4aca20b06aed661fad7
Parkhi <parkhigarg277@gmail.com>
2022-06-18 17:58:30 +0530

    error

--- routers/totalcandidates.js
+++ routers/totalcandidates.js
@@ -9,15 +9,15 @@ const router = new express.Router();
 const User = require("../schema_details/user");
 const db = require("../data_connection/data");
 
-router.get("/totalcandidates", async (req, res) => {
-  try {
-    db.users.aggregate([{ $count: "myCount" }]);
-    console.log(count);
-    res.status(200).send({ msg: "done" });
-  } catch (error) {
-    console.log(error);
-    res.status(400).send("error");
-  }
-});
+// router.get("/totalcandidates", async (req, res) => {
+//   try {
+//     db.users.aggregate([{ $count: "myCount" }]);
+//     console.log(count);
+//     res.status(200).send({ msg: "done" });
+//   } catch (error) {
+//     console.log(error);
+//     res.status(400).send("error");
+//   }
+// });
 
 module.exports = router;
