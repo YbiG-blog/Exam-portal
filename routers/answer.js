28a050d02afa344f0c6ac5e8b521a0518efb0079
Sukriti <sukritimaurya.fzd@gmail.com>
2022-06-21 23:54:16 +0530

    see_answer

--- routers/answer.js
+++ routers/answer.js
@@ -11,7 +11,7 @@ router.post("/answer", verify, async (req, res) => {
     const token = req.body.cookie_token;
 
     const dec = token.split(".")[1];
-    const decode = JSON.parse(atob(dec)); //contains userId
+    const decode = JSON.parse(atob(dec)); //contains Userid
     console.log(dec);
 
     const { question, category, userAnswer, markRev, saveNext, Qid } =
@@ -26,6 +26,23 @@ router.post("/answer", verify, async (req, res) => {
       Qid,
     });
     await answer_create.save();
+    //working on matching the correct answer
+//    const quesFound=Question.findById(Qid);
+//    //currently working on it
+//     if(quesFound)
+//     {console.log("quesFound");
+//     console.log(quesFound.options);
+// for(const i=0; i<quesFound.options.length;i++)
+//      { if(userAnswer==quesFound.options[i].value)
+//       {
+//         if(isCorrect)
+//         {
+//           Answer.findByIdAndUpdate(Qid,
+//             {$set:{isCorrect: true}});
+//         }
+
+//       }}
+//     }
 
     res.status(201).send({ msg: "Response added successfully", answer_create });
   } catch (error) {
@@ -35,9 +52,9 @@ router.post("/answer", verify, async (req, res) => {
 
 
 router.get("/seeanswer",async (req,res)=>{
-    try {const Userid = req.body.Userid;
+    try {const userId = req.body.userId;
 
-        const AnswerData = await Answer.find({userId:Userid}).populate('userId','name studentNum branch score loginAt');
+        const AnswerData = await Answer.find({userId:userId}).populate('userId','name studentNum branch score loginAt');
         res.status(201).send(AnswerData);
       } catch (err) {
         res.status(400).send(err);
