0616f86cc1ac1df9c091bbca21f8e248efdc136b
PARKHI277 <76432727+PARKHI277@users.noreply.github.com>
2022-06-26 16:50:19 +0530

    Revert "admin api"

--- schema_details/totaldata.js
+++
@@ -1,7 +0,0 @@
-const Total_admin_data = {
-    total_registration : 0,
-    total_ques_uploaded : 0,
-    total_feedback_ques : 0,
-    total_attendees : 0
-  };
-  module.exports = Total_admin_data;
