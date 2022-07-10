496baf62cf4cb248d087de1a9542f77ad18c7277
Parkhi <parkhigarg277@gmail.com>
2022-07-10 12:26:02 +0530

    totalattenes

--- routers/totalcandidates.js
+++ routers/totalcandidates.js
@@ -10,20 +10,30 @@ const User = require("../schema_details/user");
 const Feedback_Ins = require("../schema_details/ins_feed");
 const Question = require("../schema_details/question");
 
-router.get("/total", async(req,res)=>{
-    try {
-        const total_registration = await User.find().countDocuments();
-        const  total_ques_uploaded = await Question.find().countDocuments();
-        const total_feedback_ques = await Feedback_Ins.find().countDocuments();
-        const current_active_users = await User.find({ hasAppeared: true }).countDocuments();
-        const total_attendees = await User.find({ login_user : true }).countDocuments();
-        const result = {total_registration, total_ques_uploaded,total_feedback_ques, current_active_users, total_attendees};
-        console.log(result);
-        res.status(200).json({ result });
-        return;
-      } catch (err) {
-        res.sendStatus(500);
-       return;
-      }
-})
+router.get("/total", async (req, res) => {
+  try {
+    const total_registration = await User.find().countDocuments();
+    const total_ques_uploaded = await Question.find().countDocuments();
+    const total_feedback_ques = await Feedback_Ins.find().countDocuments();
+    const current_active_users = await User.find({
+      hasAppeared: true,
+    }).countDocuments();
+    const total_attendees = await User.find({
+      hasAppeared: true,
+    }).countDocuments();
+    const result = {
+      total_registration,
+      total_ques_uploaded,
+      total_feedback_ques,
+      current_active_users,
+      total_attendees,
+    };
+    console.log(result);
+    res.status(200).json({ result });
+    return;
+  } catch (err) {
+    res.sendStatus(500);
+    return;
+  }
+});
 module.exports = router;
