558aea11241a3ba45a58cc3dd4e732ccee331846
Yash Vardhan <88626469+YbiG-blog@users.noreply.github.com>
2022-07-17 23:37:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,26 +18,22 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//app.use(cors());
-//app.options('*', cors());
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "OPTION,GET,POST,PUT,PATCH,DELETE"
-//   );
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "X-Requested-With,content-type"
-//   );
-//   res.setHeader("Access-Control-Allow-Credentials", true);
-//   next();
-// });
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-  });
+app.use(cors());
+app.options('*', cors());
+ app.use((req, res, next) => {
+   res.setHeader("Access-Control-Allow-Origin", "*");
+   res.setHeader(
+     "Access-Control-Allow-Methods",
+     "OPTION,GET,POST,PUT,PATCH,DELETE"
+   );
+   res.setHeader(
+     "Access-Control-Allow-Headers",
+     "X-Requested-With,content-type"
+   );
+   res.setHeader("Access-Control-Allow-Credentials", true);
+   next();
+});
+
 /*app.use(
   cors({
     origin: "*",
