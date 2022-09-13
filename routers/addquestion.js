4acffd40557a0bd4da2b7201debae1c3adaf9eef
YbiG-blog <yashvardhan7906@gmail.com>
2022-09-13 18:56:19 +0530

    ques shuffle

--- routers/addquestion.js
+++ routers/addquestion.js
@@ -144,20 +144,6 @@ router.get("/shuffle/:category", async (req, res) => {
     for (let i of ques_category) {
       ques_array.push(await Question.findById(i._id));
     }
-    for (let i = 0; i < ques_array.length; i++) {
-    for (let k = 0; k < 4; k++) {
-      var j = Math.floor(Math.random() * (k + 1));
-      var temp = ques_array[i].options[k];
-      ques_array[i].options[k] = ques_array[i].options[j];
-      ques_array[i].options[j] = temp;
-    }
-  }
-    // for (var i = ques_array.length - 1; i > 0; i--) {
-    //   var j = Math.floor(Math.random() * (i + 1));
-    //   var temp = ques_array[i];
-    //   ques_array[i] = ques_array[j];
-    //   ques_array[j] = temp;
-    // }
 
     res.status(200).json({ result: ques_array });
   } catch (err) {
@@ -174,43 +160,102 @@ router.put("/shuffle/:category", verify, async (req, res) => {
     const dec = token.split(".")[1];
     const decode = JSON.parse(atob(dec)); //contains Userid
     console.log(dec);
+    const userFind = await User.findById(decode._id);
     const type = req.params.category;
     const ques_category = await Question.find({
       category: type,
     });
     let ques_array = [];
     for (let i of ques_category) {
-      let quesget = await Question.findById(i._id);
-      let ansmatch = await Answer.find({ Qid: i._id, userId : decode._id});
+      ques_array.push(await Question.findById(i._id));
+    }
+
+    for (var i = ques_array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = ques_array[i];
+      ques_array[i] = ques_array[j];
+      ques_array[j] = temp;
+    }
+    //     // condition for shuffling
+    if ((type == "HTML" || type == "html") && userFind.Ahtml.f == false) {
+      await User.findByIdAndUpdate(decode._id, {
+        $set: {
+          Ahtml: { f: true, val: ques_array },
+        },
+      });
+    } else if ((type == "CSS" || type == "css") && userFind.Acss.f == false) {
+      await User.findByIdAndUpdate(decode._id, {
+        $set: {
+          Acss: { f: true, val: ques_array },
+        },
+      });
+    } else if ((type == "SQL" || type == "sql") && userFind.Asql.f == false) {
+      await User.findByIdAndUpdate(decode._id, {
+        $set: {
+          Asql: { f: true, val: ques_array },
+        },
+      });
+    } else if (
+      (type == "APTITUDE" || type == "aptitude") &&
+      userFind.Aaptitude.f == false
+    ) {
+      await User.findByIdAndUpdate(decode._id, {
+        $set: {
+          Aaptitude: { f: true, val: ques_array },
+        },
+      });
+    } else if (
+      (type == "C" || type == "C++" || type == "JAVA" || type == "PYTHON") &&
+      userFind.Aother.f == false
+    ) {
+      await User.findByIdAndUpdate(decode._id, {
+        $set: {
+          Aother: { f: true, val: ques_array },
+        },
+      });
+    }
+    // shuffle questions
+    const shuffleques = await User.findById(userFind._id);
+    let qry_array = [];
+    if (type == "HTML" || type == "html") {
+      qry_array = shuffleques.Ahtml.val;
+    } else if (type == "CSS" || type == "css") {
+      qry_array = shuffleques.Acss.val;
+    } else if (type == "SQL" || type == "sql") {
+      qry_array = shuffleques.Asql.val;
+    } else if (type == "APTITUDE" || type == "aptitude") {
+      qry_array = shuffleques.Aaptitude.val;
+    } else if (
+      type == "C" ||
+      type == "C++" ||
+      type == "JAVA" ||
+      type == "PYTHON"
+    ) {
+      qry_array = shuffleques.Aother.val;
+    }
+    let finalArray = [];
+    for (let i = 0; i < qry_array.length; i++) {
+      let quesget = qry_array[i];
+      let ansmatch = await Answer.find({ Qid: qry_array[i]._id, userId: shuffleques._id });
       let ans_flagRes = {}
-      if(ansmatch.length!=0){
-      let ans_flag = {
-        // userid: ansmatch[ansmatch.length-1].userId,
-        flag : ansmatch[ansmatch.length-1].ansid,
-        setopt : ansmatch[ansmatch.length-1].selectedOpt
+      if (ansmatch.length != 0) {
+        let ans_flag = {
+          // userid: ansmatch[ansmatch.length-1].userId,
+          flag: ansmatch[ansmatch.length - 1].ansid,
+          setopt: ansmatch[ansmatch.length - 1].selectedOpt
+        }
+        ans_flagRes = ans_flag
+      } else {
+        let ans_flag = {
+          flag: 2,
+          setopt: ""
+        }
+        ans_flagRes = ans_flag
       }
-      ans_flagRes = ans_flag
-    }else
-    {
-      let ans_flag = {
-        flag : 2,
-        setopt : ""
-      }
-      ans_flagRes = ans_flag
-    }
-      ques_array.push({quesget ,ans_flagRes});
-    }
+      finalArray.push({ quesget, ans_flagRes });
 
-    for (let i = 0; i < ques_array.length; i++) {
-      for (let k = 0; k < 4; k++) {
-        var j = Math.floor(Math.random() * (k + 1));
-        var temp = ques_array[i].quesget.options[k];
-        ques_array[i].quesget.options[k] = ques_array[i].quesget.options[j];
-        ques_array[i].quesget.options[j] = temp;
-      }
     }
-
-    res.status(200).json({ result: ques_array });
+    res.status(200).json({ result: finalArray });
   } catch (err) {
     console.log(err);
     res.status(400).json(err);
